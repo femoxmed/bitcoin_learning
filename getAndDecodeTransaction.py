ab71c82cb9cb774bb66d2062f4afb8ef548eccee
Femi <oluwafemi@delyorkinternational.com>
2024-01-27 17:32:31 +0100

    first commit

---
+++ getAndDecodeTransaction.py
@@ -0,0 +1,15 @@
+from bitcoin.rpc import RawProxy
+p = RawProxy()
+txid = "466200308696215bbc949d5141a49a4138ecdfdfaa2a8029c1f9bcecd1f96177" 
+# First, retrieve the raw transaction in hex
+
+blockhash = p.getblockhash(1)
+print(blockhash)
+
+print(p.getblock(blockhash))
+raw_tx = p.getrawtransaction(txid)
+
+# # Decode the transaction hex into a JSON object
+decoded_tx = p.decoderawtransaction(raw_tx)
+# # Retrieve each of the outputs from the transaction
+for output in decoded_tx['vout']: print(output['scriptPubKey']['address'], output['value'])
